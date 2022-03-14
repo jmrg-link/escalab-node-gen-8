8a5c73e975e82e3b7a0e06c524077b7e8dcbc0d6
Jesus Maria Rico Gonzalez <contactame@jmrg.link>
2022-03-14 19:32:42 +0100

    Update user.routes.js and details

--- server/api/v1/global/routes/user.routes.js
+++ server/api/v1/global/routes/user.routes.js
@@ -1,15 +1,41 @@
-const express = require("express");
+const express = require( "express" );
 const router = express.Router();
 
-// endpoints global users
-// GET - READ
-router.get("/user/:id")
-router.get("/users")
+// [ENDPOINTS GLOBAL USER]
 
-// POST - CREATE
-router.post("/user")
+// [GET - READ]
+// [GET] - Obtiene un solo usuario por ID
+// [GET] - Perfil Usuario [ROLE | USER] : solo puede solicitarse a si mismo
+// [GET] - Perfil Usuario [ROLE | "ADMIN" - "OFFICE" ] : puede solicitar cualquier informacion de user
+router.get( "/user/:id" )
+// [GET] - Obtiene una lista de usuarios { only - administrator }
+router.get( "/users" )
 
-// DELETE - DELETE
-router.delete("/user/:id")
+// FINISH [ GET ] \\
+// [************] \\
 
+// [PUT - UPDATE]
+// [PUT] - Perfil Usuario [ROLE | GUEST] : solo puede actualizarse a si mismo datos básicos.
+// [PUT] - Perfil Usuario [ROLE | "ADMIN" - "OFFICE" ] : puede actualizar [ADMIN USER - OFFICE & GUEST ] puede actualizardatos básicos.
+// [PUT] - Perfil Usuario [ROLE | "ADMIN" - "OFFICE" ] :
+// [PUT] - Activar o Desactivar usuario por ID
+router.put( "/user/:id/active" )
+// [PUT] - Desactivar usuario por ID
+router.put( "/user/:id/desactivate" )
+
+// FINISH [ PUT ] \\
+// [************] \\
+
+// [POST - CREATE]
+// [POST] - Crear Usuario
+router.post( "/user" )
+// FINISH [ POST ] \\
+// [*************] \\
+
+
+// [DELETE - DELETE]
+router.delete( "/user/:id" )
+
+// FINISH [ DELETE ] \\
+// [***************] \\
 module.exports = router;
